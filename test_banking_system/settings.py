7daeb4c71c66dd9fa406788991fdd12e4222ad05
justine-andrie-v-abella <justineandrieabella@gmail.com>
2026-07-10 08:34:38 +0800

    added the new context processor

--- test_banking_system/settings.py
+++ test_banking_system/settings.py
@@ -51,6 +51,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.notifications_context',
+                'accounts.context_processors.school_year_context',  
             ],
         },
     },
