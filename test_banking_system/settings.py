bf16cc68fd8de54df33c5ada582b4be627180b3d
justine-andrie-v-abella <justineandrieabella@gmail.com>
2026-07-16 16:19:05 +0800

    allowed the vercel domain app

--- test_banking_system/settings.py
+++ test_banking_system/settings.py
@@ -10,7 +10,12 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-fallback-key')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['qvrbmzwj-2000.asse.devtunnels.ms/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['qvrbmzwj-2000.asse.devtunnels.ms/',
+                 'localhost', 
+                 '127.0.0.1',
+                 'testbankingsystem-6ik71xycq-justine-andrieabella-9237s-projects.vercel.app',
+                 '.vercel.app'
+                 ]
 CSRF_TRUSTED_ORIGINS = ['https://qvrbmzwj-2000.asse.devtunnels.ms/']
 
 
